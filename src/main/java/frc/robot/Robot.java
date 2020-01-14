deb3b9cf85507bea614645996793efffbf4b6f51
nathanbgray <59852185+nathanbgray@users.noreply.github.com>
2020-01-14 16:54:54 -0600

    Update Robot.java
    Made the class variables private

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,7 +15,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Encoder;
 
-
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
@@ -29,11 +28,11 @@ public class Robot extends TimedRobot {
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  CANSparkMax left = new CANSparkMax(4, MotorType.kBrushless);
-  CANSparkMax right = new CANSparkMax(5, MotorType.kBrushless);
-  Encoder shaftEncoder = new Encoder(0, 1);
-  double wheelVelocity = 0.0;
-  final double pi = 3.141592;
+  private final double pi = 3.141592;
+  private CANSparkMax left;
+  private CANSparkMax right;
+  private Encoder shaftEncoder;
+  private double wheelVelocity = 0.0;
 
   /**
    * This function is run when the robot is first started up and should be used
@@ -44,18 +43,23 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
-    
-  shaftEncoder.setDistancePerPulse((4*pi)/2048);
-  shaftEncoder.setReverseDirection(true);
+
+    left = new CANSparkMax(4, MotorType.kBrushless);
+    right = new CANSparkMax(5, MotorType.kBrushless);
+    shaftEncoder = new Encoder(0, 1);
+
+    shaftEncoder.setDistancePerPulse((4 * pi) / 2048);
+    shaftEncoder.setReverseDirection(true);
   }
 
   /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
+   * This function is called every robot packet, no matter the mode. Use this for
+   * items like diagnostics that you want ran during disabled, autonomous,
+   * teleoperated and test.
    *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and SmartDashboard integrated updating.
    */
   @Override
   public void robotPeriodic() {
@@ -63,14 +67,15 @@ public class Robot extends TimedRobot {
 
   /**
    * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
+   * between different autonomous modes using the dashboard. The sendable chooser
+   * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+   * remove all of the chooser code and uncomment the getString line to get the
+   * auto name from the text box below the Gyro
    *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
+   * <p>
+   * You can add additional auto modes by adding additional comparisons to the
+   * switch structure below with additional strings. If using the SendableChooser
+   * make sure to add them to the chooser code above as well.
    */
   @Override
   public void autonomousInit() {
@@ -85,13 +90,13 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousPeriodic() {
     switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
+    case kCustomAuto:
+      // Put custom auto code here
+      break;
+    case kDefaultAuto:
+    default:
+      // Put default auto code here
+      break;
     }
   }
 
@@ -101,19 +106,18 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
 
-
     double motorSpeed = 1.0;
     left.set(-motorSpeed);
     right.set(motorSpeed);
-    
+
     wheelVelocity = shaftEncoder.getRate();
 
-     double rps = wheelVelocity / (4 * pi);
-     double rpm = rps * 60;
-     System.out.println("Wheel Velocity (in/s): " + wheelVelocity);
-     System.out.println("rpm: " + rpm);
-     System.out.println("left motor rpm: " + left.getEncoder().getVelocity());
-     System.out.println("right motor rpm: " + right.getEncoder().getVelocity());
+    double rps = wheelVelocity / (4 * pi);
+    double rpm = rps * 60;
+    System.out.println("Wheel Velocity (in/s): " + wheelVelocity);
+    System.out.println("rpm: " + rpm);
+    System.out.println("left motor rpm: " + left.getEncoder().getVelocity());
+    System.out.println("right motor rpm: " + right.getEncoder().getVelocity());
 
   }
 
