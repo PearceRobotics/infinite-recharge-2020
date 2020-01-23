3c4b5fdb0c40d07e55497d8750239a6708d592bf
Rene-Baquero <ccbaquero@yahoo.com>
2020-01-23 17:31:38 -0600

    Changing Controls class to adjusted Controls class

--- src/main/java/frc/robot/io/Controls.java
+++ src/main/java/frc/robot/io/Controls.java
@@ -7,29 +7,28 @@
 
 package frc.robot.io;
 
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
-
 /**
  * Add your docs here.
  */
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 public class Controls {
     private Joystick joystick;
-
+    private final int LEFT_X = 0;
     private final int LEFT_Y = 1;
     private final int RIGHT_X = 4;
     private final int RIGHT_Y = 5;
 
     private final int RIGHT_BUMPER_ID = 6;
-    private final int RIGHT_TRIGGER_ID = 3;
-    
+    private final int RIGHT_TRIGGER_ID = 8;
+
     private final int LEFT_BUMPER_ID = 5;
-    private final int LEFT_TRIGGER_ID = 2;
+    private final int LEFT_TRIGGER_ID = 7;
 
     private final int B_BUTTON_ID = 2;
-    private final int Y_BUTTON_ID = 2;
-    private final int A_BUTTON_ID = 2;
-    private final int X_BUTTON_ID = 2;
+    private final int Y_BUTTON_ID = 4;
+    private final int A_BUTTON_ID = 1;
+    private final int X_BUTTON_ID = 3;
 
     public Controls(Joystick joystick) {
         this.joystick = joystick;
@@ -39,23 +38,19 @@ public class Controls {
         return joystick.getRawButton(B_BUTTON_ID);
     }
 
-    public boolean getXButton() {
-        return joystick.getRawButton(X_BUTTON_ID);
-    }
-
-    public boolean getYButton() {
-        return joystick.getRawButton(Y_BUTTON_ID);
-    }
-
     public boolean getAButton() {
         return joystick.getRawButton(A_BUTTON_ID);
     }
 
-    public JoystickButton getYJoystickButton() {
+    public boolean getXButton() {
+        return joystick.getRawButton(X_BUTTON_ID);
+    }
+
+    public JoystickButton getYJoystickButton(){
         return new JoystickButton(joystick, Y_BUTTON_ID);
     }
 
-    public JoystickButton getAJoystickButton() {
+    public JoystickButton getAJoystickButton(){
         return new JoystickButton(joystick, A_BUTTON_ID);
     }
 
@@ -63,23 +58,23 @@ public class Controls {
         double x = joystick.getRawAxis(RIGHT_X);
         if(Math.abs(x) < deadzone) {
             return 0.0;
-        }
+        } 
         return x;
-    }
-
-    public double getRightY(double deadzone) {
-        double y = joystick.getRawAxis(RIGHT_Y);
-        if(Math.abs(y) < deadzone) {
-            return 0.0;
-        }
-        return y;
     }
 
     public double getLeftY(double deadzone) {
         double y = joystick.getRawAxis(LEFT_Y);
         if(Math.abs(y) < deadzone) {
             return 0.0;
-        }
+        } 
+        return y;
+    }
+
+    public double getRightY(double deadzone) {
+        double y = joystick.getRawAxis(RIGHT_Y);
+        if(Math.abs(y) < deadzone) {
+            return 0.0;
+        } 
         return y;
     }
 
@@ -87,7 +82,7 @@ public class Controls {
         return joystick.getRawButton(RIGHT_BUMPER_ID);
     }
 
-    public JoystickButton getRightJoystickBumper() {
+    public JoystickButton getRightJoystickBumper(){
         return new JoystickButton(joystick, RIGHT_BUMPER_ID);
     }
 
@@ -102,4 +97,4 @@ public class Controls {
     public boolean getLeftTrigger() {
         return joystick.getRawAxis(LEFT_TRIGGER_ID) > 0;
     }
-}
+} 
