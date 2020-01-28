ab95a1177789fd1f43fa3e06eafe475ddc672e77
Rene-Baquero <ccbaquero@yahoo.com>
2020-01-27 18:21:23 -0600

    Garretts shooter code changes

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -17,84 +17,60 @@ public class Shooter {
 
     private final int leftMotorCanId = 11;
     private final int rightMotorCanId = 12;
-    private final double wheelCircumference = 6*Math.PI;
-    private final double maxTangentialSpeed = (2048* wheelCircumference)/60;
-    private final double Gravity = 386.0886;
-    private double launcherHeight; //make final 
-
-    private double desiredHeight = 98;
-    private double distanceToTarget;
-
-    private double deadzone;
-    private double upperBound;
-    private double lowerBound;
-
-    private double maxLaunchSpeed;
-    private double horizontalSpeed;
-    private double travelTime;
-    private double launchPower;
-    private double heightAtTargetDistance;
-    private boolean shouldShoot;
-    private double degrees;
-    private double radians;
-
     private final CANSparkMax leftMotor = new CANSparkMax(leftMotorCanId, MotorType.kBrushless);
     private final CANSparkMax rightMotor = new CANSparkMax(rightMotorCanId,MotorType.kBrushless);
 
-    public void setDegrees(double degrees){
-        this.degrees = degrees;
-    }
-
-    public void ConvertDegreestoRadians(double degrees){
-        this.radians = degrees * (Math.PI/180);
-    }
-
-    public void MaxLaunchSpeed(double energyLost)
-    {
-        this.maxLaunchSpeed = maxTangentialSpeed*(1 - energyLost);
-    }
-
-    public void horizontalSpeed(){
-     horizontalSpeed = Math.cos(radians) * maxLaunchSpeed;
-    }
-
-    public void travelTime(){
-        travelTime = distanceToTarget/horizontalSpeed;
-    }
+    private final double wheelDiameter = 4; //inch
+    private final double Gravity = 386.0886;
+    private final double maxRpm = 7640.0; 
+    private final double launcherHeight = 24.0; //inch
+    private final double energyLost = 0.66; 
+    private final double wheelCircumference = wheelDiameter*Math.PI;
+    private final double maxTangentialSpeed = ( wheelCircumference * (maxRpm /60));
+    private final double initialUpperBound = 1.0;
+    private final double initalLowerBound = 0.0;
+    private double launchPower = initialUpperBound;
+    private final double degrees = 58.0;
+    private final double radians = degrees * (Math.PI/180);
+    private double desiredHeight = 88.0;
+    private double distanceToTarget;
+    private double maxLaunchSpeed = maxTangentialSpeed*(1 - energyLost);
+    private double horizontalSpeed = Math.cos(radians) * maxLaunchSpeed * launchPower;;
+    private double travelTime = distanceToTarget/horizontalSpeed;;
+    private double heightAtTargetDistance;
+    private boolean shouldShoot;
 
     public void determineHeightAtTargetDistance(){
         heightAtTargetDistance = launcherHeight + (Math.sin(radians)* maxLaunchSpeed * launchPower *travelTime) -(0.5 * Gravity *Math.pow(travelTime, 2 ));
-        if(heightAtTargetDistance < desiredHeight - deadzone){
+        if(heightAtTargetDistance < desiredHeight){
             shouldShoot = false;
-        
         }
         else{
             shouldShoot = true;
         }
     }
     public void iteration(){
+        double upperBound = initialUpperBound;
+        double lowerBound = initalLowerBound;
         if(shouldShoot == true){
             while (heightAtTargetDistance != desiredHeight){
-             if(heightAtTargetDistance > desiredHeight){
-            launchPower = launchPower - Math.abs(upperBound -lowerBound);
-            lowerBound = upperBound - Math.abs(upperBound - lowerBound);
+                if(heightAtTargetDistance > desiredHeight){
+                    launchPower = launchPower - (Math.abs(upperBound -lowerBound)/2);
+                    lowerBound = upperBound - (Math.abs(upperBound - lowerBound)/2);
+                }
+                    else if(heightAtTargetDistance < desiredHeight){
+                        launchPower = launchPower + (Math.abs(upperBound - lowerBound)/2);
+                        upperBound = upperBound + (Math.abs(upperBound - lowerBound)/2);
+                    }
+                else{
+                    horizontalSpeed = Math.cos(radians) * maxLaunchSpeed * launchPower;
+                travelTime = distanceToTarget/ horizontalSpeed;
+                heightAtTargetDistance = launcherHeight + (Math.sin(radians)*maxLaunchSpeed*launchPower*travelTime)-(0.5 * Gravity * Math.pow(travelTime,2));
+                }
             }
-            else if(heightAtTargetDistance < desiredHeight){
-            launchPower = launchPower + Math.abs(upperBound - lowerBound);
-            upperBound = upperBound + Math.abs(upperBound - lowerBound);
-            }
-
-            horizontalSpeed = Math.cos(radians) * maxLaunchSpeed * launchPower;
-            travelTime = distanceToTarget/ horizontalSpeed;
-            heightAtTargetDistance = launcherHeight + (Math.sin(radians)*maxLaunchSpeed*launchPower*travelTime)-(0.5 * Gravity * Math.pow(travelTime,2));
-         }
-
-
         }
         else{
-
+        //Shot is not possible
         }
     }
-
-
 }
