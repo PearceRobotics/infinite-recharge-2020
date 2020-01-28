f2960af6bafa241b6f323081ed340e88b2d90172
Eliza Barnett <elizacatbarnett@gmail.com>
2020-01-27 20:31:39 -0600

    started formatting

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -29,38 +29,36 @@ public class Drive {
     private final int RIGHT_FRONT_CAN_ID = 5;
 
     private MotorType DRIVE_MOTOR_TYPE = MotorType.kBrushless;
+
     public Drive() {
-        this.leftGearbox = new Gearbox(new CANSparkMax(LEFT_BACK_CAN_ID, DRIVE_MOTOR_TYPE), 
-                                        new CANSparkMax(LEFT_FRONT_CAN_ID, DRIVE_MOTOR_TYPE));
-    
-        this.rightGearbox = new Gearbox(new CANSparkMax(RIGHT_BACK_CAN_ID, DRIVE_MOTOR_TYPE),
-                                        new CANSparkMax(RIGHT_FRONT_CAN_ID, DRIVE_MOTOR_TYPE));
-        this.leftGearbox.setRampRate(1);
-        this.rightGearbox.setRampRate(1);
-        
+      this.leftGearbox = new Gearbox(new CANSparkMax(LEFT_BACK_CAN_ID, DRIVE_MOTOR_TYPE), 
+                                      new CANSparkMax(LEFT_FRONT_CAN_ID, DRIVE_MOTOR_TYPE));
+  
+      this.rightGearbox = new Gearbox(new CANSparkMax(RIGHT_BACK_CAN_ID, DRIVE_MOTOR_TYPE),
+                                      new CANSparkMax(RIGHT_FRONT_CAN_ID, DRIVE_MOTOR_TYPE));
+      this.leftGearbox.setRampRate(1);
+      this.rightGearbox.setRampRate(1);
 
-    this.leftEncoder = new Encoder(0, 1);
-    this.rightEncoder = new Encoder(4, 5);
+      this.leftEncoder = new Encoder(0, 1);
+      this.rightEncoder = new Encoder(4, 5);
 
-    leftEncoder.setDistancePerPulse((6.0*Math.PI)/2048.0);
-    leftEncoder.setReverseDirection(true);
-    rightEncoder.setDistancePerPulse((6.0*Math.PI)/2048.0);
-      }
+      leftEncoder.setDistancePerPulse((6.0*Math.PI)/2048.0);
+      leftEncoder.setReverseDirection(true);
+      rightEncoder.setDistancePerPulse((6.0*Math.PI)/2048.0);
+   }
     
-      public void setLeftSpeed(double speed) {
-        this.leftGearbox.setSpeed(speed);
-      }
+    public void setLeftSpeed(double speed) {
+      this.leftGearbox.setSpeed(speed);
+    }
     
-      public void setRightSpeed(double speed) {
-        this.rightGearbox.setSpeed(speed);
-      }
+    public void setRightSpeed(double speed) {
+      this.rightGearbox.setSpeed(speed);
+    }
     
-      public void arcadeDrive(double staightSpeed, double turnModifer) {
-        this.setLeftSpeed(-(staightSpeed - turnModifer));
-        this.setRightSpeed(staightSpeed + turnModifer);
-
-        
-      }
+    public void arcadeDrive(double staightSpeed, double turnModifer) {
+      this.setLeftSpeed(-(staightSpeed - turnModifer));
+      this.setRightSpeed(staightSpeed + turnModifer); 
+    }
     
       public void arcadeDrive(DrivingDeltas drivingDeltas) {
         arcadeDrive(drivingDeltas.getForwardPower(), drivingDeltas.getSteeringPower());
