163246d6c8dde1f78995c492b68b466b30528869
Kusha Gharahi <kusha.gharahi@toyotaconnected.com>
2020-01-30 21:05:23 -0600

    trying threading (not working atm)

--- src/main/java/frc/robot/subsystems/Lights.java
+++ src/main/java/frc/robot/subsystems/Lights.java
@@ -1,5 +1,8 @@
 package frc.robot.subsystems;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.util.Color8Bit;
@@ -15,6 +18,8 @@ public class Lights {
     private final Color8Bit BLUE = new Color8Bit(0, 0, 139);
     private final Color8Bit LIME_GREEN = new Color8Bit(0, 255, 0);
 
+    private ThreadPoolExecutor executor;
+
     public Lights(int pwmPort, int numLeds, long delay) {
         ledStrip = new AddressableLED(pwmPort);
         ledBuffer = new AddressableLEDBuffer(numLeds);
@@ -22,6 +27,7 @@ public class Lights {
         ledStrip.setLength(ledBuffer.getLength());
         ledStrip.setData(ledBuffer);
         ledStrip.start();
+        executor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
     }
 
     public void allBlue() {
@@ -41,22 +47,27 @@ public class Lights {
     }
 
     public void idleAnimation(int length) {
-        for (int x = ledBuffer.getLength() - length; x > 1; x--) {
-            for(int y = 0; y < length; y++) {
-                ledBuffer.setLED((ledBuffer.getLength() - length) - x + y, RED);
-                ledBuffer.setLED(x + y, RED);
-            }
-            ledStrip.setData(ledBuffer);
-            for(int y = 0; y < length; y++) {
-                ledBuffer.setLED((ledBuffer.getLength()-length) - x + y, BLUE);
-                ledBuffer.setLED(x + y, BLUE);
-            }
-            try {
-                Thread.sleep(delay);
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+        if(executor.getPoolSize() == 0) {
+            executor.submit(() -> {
+                for (int x = ledBuffer.getLength() - length; x > 1; x--) {
+                    for(int y = 0; y < length; y++) {
+                        ledBuffer.setLED((ledBuffer.getLength() - length) - x + y, RED);
+                        ledBuffer.setLED(x + y, RED);
+                    }
+                    ledStrip.setData(ledBuffer);
+                    for(int y = 0; y < length; y++) {
+                        ledBuffer.setLED((ledBuffer.getLength()-length) - x + y, BLUE);
+                        ledBuffer.setLED(x + y, BLUE);
+                    }
+                    try {
+                        Thread.sleep(delay);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+                executor.shutdown();
+            });
         }
     }
 }
