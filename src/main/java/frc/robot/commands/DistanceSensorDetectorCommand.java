927902b3653e2b368f794f6042b3f14d529d3ef0
Rene-Baquero <ccbaquero@yahoo.com>
2020-02-05 17:47:18 -0600

    Actual code for distance sensor

--- src/main/java/frc/robot/commands/DistanceSensorDetectorCommand.java
+++ src/main/java/frc/robot/commands/DistanceSensorDetectorCommand.java
@@ -8,7 +8,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import com.revrobotics.*;
@@ -18,20 +17,27 @@ import com.revrobotics.Rev2mDistanceSensor.Port;
  * Add your docs here.
  */
 public class DistanceSensorDetectorCommand extends CommandBase{
+    private Rev2mDistanceSensor distOnboard; 
     
     public DistanceSensorDetectorCommand()
     {
-
+        //Empty constructor might add more later
     }
 
     //Called just before this command runs for the first time
     @Override
     public void initialize() {
-
+        distOnboard = new Rev2mDistanceSensor(Port.kOnboard);
+        distOnboard.setAutomaticMode(true);
     }
 
+    //Called and executes during the run
     @Override
     public void execute() {
+        if(distOnboard.isRangeValid()) {
+            SmartDashboard.putNumber("Range Onboard", distOnboard.getRange());
+            SmartDashboard.putNumber("Timestamp Onboard", distOnboard.getTimestamp());
+          }
 
     }
 
