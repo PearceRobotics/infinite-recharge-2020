6bae2c1efd094c59e7aaa811763c31926eadbec0
nathan <nathan.b.gray2@gmail.com>
2020-02-16 20:26:05 -0600

    New Air Resistance Equation

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -6,22 +6,29 @@ import io.github.oblarg.oblog.annotations.Config;
  * Add your docs here.
  */
 public class Shooter {
-    private final double wheelDiameter = 4; // inches
-    private final double Gravity = 386.0886; //inches per second squared
-    private final double maxRpm = 7640.0; //rpms
-    private final double launcherHeight = 24.0; // inches
-    private double energyLostBase = 0.5; // Percent energy lost from full tangential speed to actual ball speed
-    private final double wheelCircumference = wheelDiameter * Math.PI; //inches
-    private final double maxTangentialSpeed = (wheelCircumference * (maxRpm / 60.0)); //inches per second
+    private final double wheelDiameter = 4.0; // in
+    private final double Gravity = 386.0886; // in/s^2
+    private final double maxRpm = 7640.0; // rpms
+    private final double launcherHeight = 23.0; // in
+    private final double wheelCircumference = wheelDiameter * Math.PI; // inches
+    private final double maxTangentialSpeed = (wheelCircumference * (maxRpm / 60.0)); // in/s
     private final double initialUpperBound = 1.0; // 100% speed
     private final double initialLowerBound = 0.0;// 0% speed
-    private final double launcherDegrees = 32.0; //launcherDegrees
-    private final double launcherRadians = Math.toRadians(launcherDegrees);
-    private double targetHeight = 98.0;
+    private final double launcherDegrees = 32.0; // launcherDegrees, used for ease of measurement and understanding
+    private final double launcherRadians = Math.toRadians(launcherDegrees); // radians
+    private final double targetHeight = 98.0; // inches above ground
 
-    private final double INCHES_TO_METERS = 2.54 / 100.0;
+    private final double ballDiameter = 7.0; // in
+    private final double rho = 0.00004428; // lbm/in^3, STP air density
+    private final double ballArea = (ballDiameter / 2.0) * (ballDiameter / 2.0) * Math.PI; // in^2
+    private final double cd = 0.15; // unitless, taken from Chief Delphi 2012 game piece calculations
+    private final double ballMass = 5.0 / 16.0; // lbm, AndyMark says balls are ~5 ounces
 
-    private double maxLaunchSpeed = maxTangentialSpeed * (1.0 - energyLostBase);
+    private final int iterations = 20; //number of iterations when searching for the solution
+
+    //not final so we can set with shuffleboard
+    private double energyLostBase = 0.5; // Percent energy lost from full tangential speed to actual ball speed
+    private double maxLaunchSpeed = maxTangentialSpeed * (1.0 - energyLostBase); // in/s
 
     public Shooter() {
         // empty contructor
@@ -30,6 +37,7 @@ public class Shooter {
     @Config
     public void setEnergyLost(double energyLostBase) {
         this.energyLostBase = energyLostBase;
+        this.maxLaunchSpeed = maxTangentialSpeed * (1.0 - energyLostBase);
     }
 
     public double determineLaunchSpeed(double distanceToTarget) {
@@ -49,7 +57,7 @@ public class Shooter {
         }
         // iteration
         else {
-            for (int i = 0; i < 15; i++) {// binary search...gets close enough to what we want
+            for (int i = 0; i < iterations; i++) {// binary search...gets close enough to what we want
                 if (heightAtTargetDistance > targetHeight) {// if our current height is greater than our target height
                                                             // raise lowerbound
                     launchPower = launchPower - (range / (2 * Math.pow(2, i))); // decreases launchPower
@@ -66,60 +74,38 @@ public class Shooter {
     }
 
     private double getHeightAtTargetDistance(double distanceToTarget, double launchPower) {
-        double horizontalSpeed;
-        double travelTime;
-
-        double ballDiameterInches = 7.0;
-        double ballDiameterMeters = ballDiameterInches * INCHES_TO_METERS;
-
-        double beta = 0.00016;
-        double gamma = 0.25;
-
-        // 155 inches is 9977017 //speed 1122
-
-        // 184 inches is 9,244,452 //speed 1080
-
         // if you have a triangle, with the hypnotonuse being the angled velocity
         // vector, the adjacent being the horizontal velocity vector, and the opposite
         // side being the height
         // horizontal speed = adjacent side -> cos(angle) = a/h -> hcos(angle) = a ->
         // (maxLaunchSpeed * launchPower)cos(angle) = a
-        horizontalSpeed = Math.cos(launcherRadians) * maxLaunchSpeed * launchPower;
+        double horizontalSpeed = Math.cos(launcherRadians) * maxLaunchSpeed * launchPower;
 
-        double horizontalSpeedMs = horizontalSpeed * INCHES_TO_METERS;
+        // air resistance
+        // 1/2 * rho * v^2 * A * cd, or lbm*in^2*in^2/in^3*s^2 === lbm*in/s^2
+        double dragForceHorizontal = 0.5 * rho * horizontalSpeed * horizontalSpeed * ballArea * cd;
 
-        double horizontalAirResistanceMetersPerSecondSquared = (ballDiameterMeters * beta * horizontalSpeedMs)
-                + (gamma * ballDiameterMeters * ballDiameterMeters * horizontalSpeedMs * horizontalSpeedMs);
+        //divide by mass to get in/s^2
+        double horizontalAirResistanceAcceleration = dragForceHorizontal / ballMass;
 
-        double horizontalAirResistanceInchesPerSecondSquared = horizontalAirResistanceMetersPerSecondSquared
-                / INCHES_TO_METERS;
+        // Use quadratic equation to find the positive solution
+        double travelTime = (-horizontalSpeed + Math.sqrt((horizontalSpeed * horizontalSpeed)
+                - (4.0 * -0.5 * horizontalAirResistanceAcceleration * -distanceToTarget)))
+                / (2.0 * -0.5 * horizontalAirResistanceAcceleration);
 
-        double qPlus = (-horizontalSpeed + Math.sqrt((horizontalSpeed * horizontalSpeed)
-                - (4.0 * -0.5 * horizontalAirResistanceInchesPerSecondSquared * -distanceToTarget)))
-                / (2.0 * -0.5 * horizontalAirResistanceInchesPerSecondSquared);
+        double verticalSpeed = (Math.sin(launcherRadians) * maxLaunchSpeed * launchPower);
 
-        // v = d/t -> vt=d -> t = d/v
-        travelTime = qPlus;// distanceToTarget / horizontalSpeed;
-
-        // System.out.println("travel time " + travelTime);
-
-        double verticalSpeedMetersPerSecond = (Math.sin(launcherRadians) * maxLaunchSpeed * launchPower) * INCHES_TO_METERS;
-
-        // System.out.println("vertical m/s " + verticalSpeedMetersPerSecond);
-
-        double verticalAirResistanceMetersPerSecondSquared = (ballDiameterMeters * beta * verticalSpeedMetersPerSecond)
-                + (gamma * ballDiameterMeters * ballDiameterMeters * verticalSpeedMetersPerSecond
-                        * verticalSpeedMetersPerSecond);
-
-        double verticalAirResistanceInchesPerSecondSquared = verticalAirResistanceMetersPerSecondSquared
-                / INCHES_TO_METERS;
-
-        // System.out.println("vertical air resistance time " +
-        // verticalAirResistanceInchesPerSecondSquared);
+        // air resistance
+        // 1/2 * rho * v^2 * A * cd, or lbm*in^2*in^2/in^3*s^2 === lbm*in/s^2
+        double dragForceVertical = 0.5 * rho * verticalSpeed * verticalSpeed * ballArea * cd;
+        
+        //divide by mass to get in/s^2
+        double verticalAirResistanceAcceleration = dragForceVertical / ballMass;
 
         // current distance = original distance +vt + .5at^2
-        double heightAtTargetDistance = launcherHeight + (Math.sin(launcherRadians) * maxLaunchSpeed * launchPower * travelTime)
-                - (0.5 * (Gravity + verticalAirResistanceInchesPerSecondSquared) * Math.pow(travelTime, 2));
+        double heightAtTargetDistance = launcherHeight
+                + (Math.sin(launcherRadians) * maxLaunchSpeed * launchPower * travelTime)
+                - (0.5 * (Gravity + verticalAirResistanceAcceleration) * Math.pow(travelTime, 2));
         return heightAtTargetDistance;
     }
 
