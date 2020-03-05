2e0d8edc9d4495216824f3d748fcece8204a23d6
Kusha Gharahi <kusha.gharahi@toyotaconnected.com>
2020-03-04 18:54:46 -0600

    aim and shoot at the same time

--- src/main/java/frc/robot/commands/PowerCellScoringCommandGroup.java
+++ src/main/java/frc/robot/commands/PowerCellScoringCommandGroup.java
@@ -1,23 +1,22 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.subsystems.DistanceSensorDetector;
 import frc.robot.subsystems.HopperController;
 import frc.robot.subsystems.IndexerController;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.shooter.ShooterSpeedController;
 import frc.robot.subsystems.vision.Limelight;
-import frc.robot.subsystems.DistanceSensorDetector;
 
 /**
  * A complex scoring command that drives forward, aims the robot to the target
  * then Launches up to five power cells at the inner zone
  */
-public class PowerCellScoringCommandGroup extends SequentialCommandGroup {
+public class PowerCellScoringCommandGroup extends ParallelCommandGroup {
 
     public PowerCellScoringCommandGroup(Drive drive, Limelight limelight, ShooterSpeedController shooterSpeedController,
             HopperController hopperController, IndexerController indexerController, DistanceSensorDetector distanceSensorDetector) {
-        addCommands(new TurnToTopTargetCommand(drive, limelight),
+        addCommands(new AimCommand(drive, limelight),
                 new ShooterCommand(shooterSpeedController, hopperController, indexerController, limelight, distanceSensorDetector));
         addRequirements(drive);
     }
