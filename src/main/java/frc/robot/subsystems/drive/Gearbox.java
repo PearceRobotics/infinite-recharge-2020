dcb07df6de7fa7d60aa0b39734836db2f56af649
Eliza Barnett <elizacatbarnett@gmail.com>
2020-03-06 12:24:05 -0600

    stuff I'll test once mechanical stops touching the robot

--- src/main/java/frc/robot/subsystems/drive/Gearbox.java
+++ src/main/java/frc/robot/subsystems/drive/Gearbox.java
@@ -8,23 +8,23 @@ import com.revrobotics.CANSparkMax.IdleMode;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 
 public class Gearbox {
-   // private CANSparkMax[] controllers;
-    private CANSparkMax canSparkMaxFront;
+    private CANSparkMax[] controllers;
+   /* private CANSparkMax canSparkMaxFront;
     private CANSparkMax canSparkMaxMiddle;
-    private CANSparkMax canSparkMaxBack;
+    private CANSparkMax canSparkMaxBack;*/
 
     private SpeedControllerGroup speedControllerGroup;
 
-    public Gearbox(/*CANSparkMax controller, CANSparkMax... controllers*/ CANSparkMax canSparkMaxFront,  CANSparkMax canSparkMaxMiddle, CANSparkMax canSparkMaxBack) {
-       /* this.controllers = controllers;
+    public Gearbox(CANSparkMax controller, CANSparkMax... controllers /*CANSparkMax canSparkMaxFront,  CANSparkMax canSparkMaxMiddle, CANSparkMax canSparkMaxBack*/) {
+        this.controllers = controllers;
         this.speedControllerGroup = new SpeedControllerGroup(controller, controllers);
         this.controllers = Arrays.copyOf(controllers, controllers.length + 1);
-        this.controllers[this.controllers.length-1] = controller;*/
-        this.canSparkMaxFront = canSparkMaxFront;
+        this.controllers[this.controllers.length-1] = controller;
+        /*this.canSparkMaxFront = canSparkMaxFront;
         this.canSparkMaxMiddle = canSparkMaxMiddle;
-        this.canSparkMaxBack = canSparkMaxBack;
+        this.canSparkMaxBack = canSparkMaxBack;*/
         
-        this.speedControllerGroup = new SpeedControllerGroup(this.canSparkMaxFront, this.canSparkMaxMiddle, this.canSparkMaxBack);
+       // this.speedControllerGroup = new SpeedControllerGroup(this.canSparkMaxFront, this.canSparkMaxMiddle, this.canSparkMaxBack);
     }
 
     public void setSpeed(double rate) {
@@ -41,29 +41,29 @@ public class Gearbox {
     }
 
     public void setRampRate(final double rate) {
-        /*for(CANSparkMax controller: this.controllers) {
+        for(CANSparkMax controller: this.controllers) {
             controller.setOpenLoopRampRate(rate);
-        }*/
-        canSparkMaxFront.setOpenLoopRampRate(rate);
+        }
+        /*canSparkMaxFront.setOpenLoopRampRate(rate);
         canSparkMaxMiddle.setOpenLoopRampRate(rate);        
-        canSparkMaxMiddle.setOpenLoopRampRate(rate);     
+        canSparkMaxMiddle.setOpenLoopRampRate(rate); */    
     }
 
     public void setBrakeMode() {
-        /*for(CANSparkMax controller: this.controllers) {
+        for(CANSparkMax controller: this.controllers) {
             controller.setIdleMode(IdleMode.kBrake);
-        }*/
-        canSparkMaxFront.setIdleMode(IdleMode.kBrake);
+        }
+        /*canSparkMaxFront.setIdleMode(IdleMode.kBrake);
         canSparkMaxMiddle.setIdleMode(IdleMode.kBrake);      
-        canSparkMaxMiddle.setIdleMode(IdleMode.kBrake);
+        canSparkMaxMiddle.setIdleMode(IdleMode.kBrake);*/
     }
 
     public void setCoastMode() {
-        /*for(CANSparkMax controller: this.controllers) {
+        for(CANSparkMax controller: this.controllers) {
             controller.setIdleMode(IdleMode.kCoast);
-        }*/
-        canSparkMaxFront.setIdleMode(IdleMode.kCoast);
+        }
+        /*canSparkMaxFront.setIdleMode(IdleMode.kCoast);
         canSparkMaxMiddle.setIdleMode(IdleMode.kCoast);      
-        canSparkMaxMiddle.setIdleMode(IdleMode.kCoast);
+        canSparkMaxMiddle.setIdleMode(IdleMode.kCoast);*/
     }
 }
